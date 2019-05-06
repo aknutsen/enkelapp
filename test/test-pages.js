d03f9a0652fa5d936b860fab6a4f501d0d630b41
aknutsen <andergk@gmail.com>
2019-05-06 10:09:25 +0200

    lagt til enhestest

--- test/test-pages.js
+++ test/test-pages.js
@@ -3,7 +3,7 @@ var request = require('request');
 
 it('Main page content', function(done) {
     request('https://infinite-reef-28548.herokuapp.com/' , function(error, response, body) {
-        expect(body).to.contain('Hello World');
+        expect(body).to.contain('Hello');
         done();
     });
 });
